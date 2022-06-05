a64d7714a96c605576600825de58b83de88d3df5
TeaCupp <nastyalivesindream@gmail.com>
2022-06-05 22:23:26 +0300

    add purchase and receipt

---
+++ test/expense/src/main/java/com/example/codeengine/expense/repository/OptionRepository.java
@@ -0,0 +1,14 @@
+package com.example.codeengine.expense.repository;
+
+import com.example.codeengine.expense.model.Category;
+import com.example.codeengine.expense.model.Option;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface OptionRepository extends JpaRepository<Option, Long> {
+
+    Category findByName(String name);
+}
