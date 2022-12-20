13cf7e637ad371a816fcd63692ae96fa920b4fe6
TeaCupp <nastyalivesindream@gmail.com>
2022-12-20 11:58:10 +0200

    Teacher Get, Put, Post, Delete

--- test/expense/src/main/java/com/example/codeengine/expense/controller/TeacherController.java
+++ test/expense/src/main/java/com/example/codeengine/expense/controller/TeacherController.java
@@ -1,6 +1,7 @@
 package com.example.codeengine.expense.controller;
 
 import com.example.codeengine.expense.model.Student;
+import com.example.codeengine.expense.model.Teacher;
 import com.example.codeengine.expense.repository.TeacherRepository;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -22,35 +23,35 @@ public class TeacherController {
         this.teacherRepository = teacherRepository;
     }
 
-    @GetMapping("/students")
-    Collection<Student> students(){
-        return studentRepository.findAll();
+    @GetMapping("/teachers")
+    Collection<Teacher> teachers(){
+        return teacherRepository.findAll();
     }
 
-    @GetMapping("student/{id}")
-    ResponseEntity<?> getStudent(@PathVariable Long id){
-        Optional<Student> student = studentRepository.findById(id);
-        return student.map(response -> ResponseEntity.ok().body(response))
+    @GetMapping("teacher/{id}")
+    ResponseEntity<?> getTeacher(@PathVariable Long id){
+        Optional<Teacher> teacher = teacherRepository.findById(id);
+        return teacher.map(response -> ResponseEntity.ok().body(response))
                 .orElse(new ResponseEntity<>(HttpStatus.NOT_FOUND));
     }
 
 
-    @PostMapping("/student")
-    ResponseEntity<Student> createStudent(@Valid @RequestBody Student student) throws URISyntaxException {
-        Student result = studentRepository.save(student);
+    @PostMapping("/teacher")
+    ResponseEntity<Teacher> createTeacher(@Valid @RequestBody Teacher teacher) throws URISyntaxException {
+        Teacher result = teacherRepository.save(teacher);
         return ResponseEntity.created(new URI("/api/student" + result.getId())).body(result);
     }
 
 
-    @PutMapping("/student/{id}")
-    ResponseEntity<Student> updateStudent(@Valid @RequestBody Student student){
-        Student result = studentRepository.save(student);
+    @PutMapping("/teacher/{id}")
+    ResponseEntity<Teacher> updateTeacher(@Valid @RequestBody Teacher teacher){
+        Teacher result = teacherRepository.save(teacher);
         return ResponseEntity.ok().body(result);
     }
 
-    @DeleteMapping("/student/{id}")
-    ResponseEntity<?> deleteStudent(@PathVariable Long id){
-        studentRepository.deleteById(id);
+    @DeleteMapping("/teacher/{id}")
+    ResponseEntity<?> deleteTeacher(@PathVariable Long id){
+        teacherRepository.deleteById(id);
         return ResponseEntity.ok().build();
     }
 }
