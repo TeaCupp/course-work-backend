3b9a14ce66e42cedea7c658a152bea376efc69a6
TeaCupp <nastyalivesindream@gmail.com>
2022-12-20 11:46:42 +0200

    Schedule, Group Get, Put, Post, Delete

--- test/expense/src/main/java/com/example/codeengine/expense/controller/GroupController.java
+++ test/expense/src/main/java/com/example/codeengine/expense/controller/GroupController.java
@@ -1,6 +1,7 @@
 package com.example.codeengine.expense.controller;
 
 import com.example.codeengine.expense.model.Faculty;
+import com.example.codeengine.expense.model.Group;
 import com.example.codeengine.expense.repository.GroupRepository;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -24,36 +25,34 @@ public class GroupController {
 
 
     @GetMapping("/groups")
-    Collection<G> faculties(){
-        return facultyRepository.findAll();
+    Collection<Group> groups(){
+        return groupRepository.findAll();
     }
 
-    @GetMapping("faculty/{id}")
-    ResponseEntity<?> getFaculty(@PathVariable Long id){
-        Optional<Faculty> faculty = facultyRepository.findById(id);
-        return faculty.map(response -> ResponseEntity.ok().body(response))
+    @GetMapping("group/{id}")
+    ResponseEntity<?> getGroup(@PathVariable Long id){
+        Optional<Group> group = groupRepository.findById(id);
+        return group.map(response -> ResponseEntity.ok().body(response))
                 .orElse(new ResponseEntity<>(HttpStatus.NOT_FOUND));
-
-
     }
 
 
-    @PostMapping("/faculty")
-    ResponseEntity<Faculty> createFaculty(@Valid @RequestBody Faculty faculty) throws URISyntaxException {
-        Faculty result = facultyRepository.save(faculty);
-        return ResponseEntity.created(new URI("/api/faculty" + result.getId())).body(result);
+    @PostMapping("/group")
+    ResponseEntity<Group> createGroup(@Valid @RequestBody Group group) throws URISyntaxException {
+        Group result = groupRepository.save(group);
+        return ResponseEntity.created(new URI("/api/group" + result.getId())).body(result);
     }
 
 
-    @PutMapping("/faculty/{id}")
-    ResponseEntity<Faculty> updateFaculty(@Valid @RequestBody Faculty faculty){
-        Faculty result = facultyRepository.save(faculty);
+    @PutMapping("/group/{id}")
+    ResponseEntity<Group> updateGroup(@Valid @RequestBody Group group){
+        Group result = groupRepository.save(group);
         return ResponseEntity.ok().body(result);
     }
 
-    @DeleteMapping("/faculty/{id}")
-    ResponseEntity<?> deleteFaculty(@PathVariable Long id){
-        facultyRepository.deleteById(id);
+    @DeleteMapping("/group/{id}")
+    ResponseEntity<?> deleteGroup(@PathVariable Long id){
+        groupRepository.deleteById(id);
         return ResponseEntity.ok().build();
     }
 }
--- test/expense/src/main/java/com/example/codeengine/expense/controller/ScheduleController.java
+++ test/expense/src/main/java/com/example/codeengine/expense/controller/ScheduleController.java
@@ -1,8 +1,17 @@
 package com.example.codeengine.expense.controller;
 
+import com.example.codeengine.expense.model.Group;
+import com.example.codeengine.expense.model.Schedule;
 import com.example.codeengine.expense.repository.ScheduleRepository;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.util.Collection;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/api")
@@ -15,4 +24,35 @@ public class ScheduleController {
     }
 
 
+    @GetMapping("/schedules")
+    Collection<Schedule> schedules(){
+        return scheduleRepository.findAll();
+    }
+
+    @GetMapping("schedule/{id}")
+    ResponseEntity<?> getSchedule(@PathVariable Long id){
+        Optional<Schedule> schedule = scheduleRepository.findById(id);
+        return schedule.map(response -> ResponseEntity.ok().body(response))
+                .orElse(new ResponseEntity<>(HttpStatus.NOT_FOUND));
+    }
+
+
+    @PostMapping("/schedule")
+    ResponseEntity<Schedule> createSchedule(@Valid @RequestBody Schedule schedule) throws URISyntaxException {
+        Schedule result = scheduleRepository.save(schedule);
+        return ResponseEntity.created(new URI("/api/schedule" + result.getId())).body(result);
+    }
+
+
+    @PutMapping("/schedule/{id}")
+    ResponseEntity<Schedule> updateSchedule(@Valid @RequestBody Schedule schedule){
+        Schedule result = scheduleRepository.save(schedule);
+        return ResponseEntity.ok().body(result);
+    }
+
+    @DeleteMapping("/schedule/{id}")
+    ResponseEntity<?> deleteSchedule(@PathVariable Long id){
+        scheduleRepository.deleteById(id);
+        return ResponseEntity.ok().build();
+    }
 }
