ac42497110ecac5702d8dbca6a7b329fa709dd7a
TeaCupp <nastyalivesindream@gmail.com>
2022-12-21 15:20:03 +0200

    add repository annotations

--- test/expense/src/main/java/com/example/codeengine/expense/repository/TeacherRepository.java
+++ test/expense/src/main/java/com/example/codeengine/expense/repository/TeacherRepository.java
@@ -2,6 +2,9 @@ package com.example.codeengine.expense.repository;
 
 import com.example.codeengine.expense.model.Teacher;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
 
 public interface TeacherRepository extends JpaRepository<Teacher, Long> {
 
