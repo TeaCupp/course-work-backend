4456123a0c08933006dd636eacc3d62502c80299
TeaCupp <nastyalivesindream@gmail.com>
2022-12-21 15:31:06 +0200

    add @joincolums in group

--- test/expense/src/main/java/com/example/codeengine/expense/model/Group.java
+++ test/expense/src/main/java/com/example/codeengine/expense/model/Group.java
@@ -5,10 +5,7 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
-import javax.persistence.Entity;
-import javax.persistence.Id;
-import javax.persistence.ManyToOne;
-import javax.persistence.Table;
+import javax.persistence.*;
 
 @AllArgsConstructor
 @NoArgsConstructor
@@ -27,7 +24,10 @@ public class Group {
 
     private int course;
 
-    @ManyToOne
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumns({
+            @JoinColumn(name="department_id", referencedColumnName="id")
+    })
     private Department department;
 
 }
