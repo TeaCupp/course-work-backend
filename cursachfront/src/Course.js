64658e20545b665f863edc7ab8d5eda4c7d59130
TeaCupp <nastyalivesindream@gmail.com>
2022-12-23 22:12:25 +0200

    Edit Course.js

---
+++ cursachfront/src/Course.js
@@ -0,0 +1,40 @@
+import React, {Component} from 'react';
+import AppNav from "./AppNav";
+
+
+class Course extends Component {
+    state = {
+        isLoading :true,
+        Courses : []
+    }
+
+    async componentDidMount() {
+        const response = await fetch('/api/courses');
+        const body = await response.json();
+        this.setState({Options: body, isLoading: false});
+    }
+
+    render() {
+        const {Courses, isLoading} = this.state;
+        if(isLoading)
+            return(<div>Loading...</div>);
+
+
+        return(
+            <div>
+                <AppNav/>
+                <h2>Course</h2>
+                {
+                    Courses.map( course =>
+                        <div id={course.id}>
+                            {course.name}
+                        </div>
+                    )
+                }
+
+            </div>
+        );
+    }
+}
+
+export default Option;
