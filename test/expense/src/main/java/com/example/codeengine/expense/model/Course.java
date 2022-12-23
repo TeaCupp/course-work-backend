df2b5042567c706dd2a9da976c87a4cbd776cc72
TeaCupp <nastyalivesindream@gmail.com>
2022-12-23 18:24:00 +0200

    Add inf to Course class

--- test/expense/src/main/java/com/example/codeengine/expense/model/Course.java
+++ test/expense/src/main/java/com/example/codeengine/expense/model/Course.java
@@ -1,4 +1,26 @@
 package com.example.codeengine.expense.model;
 
+import com.sun.istack.NotNull;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Data
+@Table(name="classroom")
+
 public class Course {
+    @Id
+    @GeneratedValue
+    private Long id;
+
+    @NotNull
+    private String name;
 }
