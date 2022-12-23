41a80baae811ebbf145a2de2e670394575b4bca5
TeaCupp <nastyalivesindream@gmail.com>
2022-12-23 17:55:36 +0200

    Departmants extend Component

---
+++ cursachfront/src/Departments.js
@@ -0,0 +1,11 @@
+import React, {Component, Fragment} from "react";
+
+
+class Departments extends Component {
+    state = { }
+    render() {
+        return ( <h2>Departments</h2> );
+    }
+}
+
+export default Departments;
