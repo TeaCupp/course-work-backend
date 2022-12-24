1bf0f4777dd3e532aadee202dcff0f55a0ce2c3a
TeaCupp <nastyalivesindream@gmail.com>
2022-12-24 13:14:31 +0200

    ReadOnlyRow in Department component folder

---
+++ cursachfront/src/DepartmentComponents/ReadOnlyRow.js
@@ -0,0 +1,27 @@
+import React from "react";
+import Moment from "react-moment";
+import {Button} from "reactstrap";
+
+const ReadOnlyRow = ({ department, handleSubmit, remove }) => {
+    return (
+        <tr className="trExpense">
+            <td className="tdExpense">{department.name}</td>
+            <td className="tdExpense">{department.shortName}</td>
+
+
+
+            <td className="tdExpense">
+                <button
+                    type="button"
+                    onClick={(event) => handleSubmit(event, department)}
+                >
+                    Edit
+                </button>
+
+                <Button size="sm" color="danger" onClick={ () => remove(department.id)}>Delete</Button>
+            </td>
+        </tr>
+    );
+};
+
+export default ReadOnlyRow;
